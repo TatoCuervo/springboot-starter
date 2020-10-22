0f8c3b3340cad9f1e0a028f487d885832aadc1e9
Cristian Lopez <lopezcristianj@gmail.com>
2020-10-22 18:04:55 +1300

    Add Lessons controller

--- src/main/java/com/tatocuervo/springbootstarter/lesson/LessonController.java
+++ src/main/java/com/tatocuervo/springbootstarter/lesson/LessonController.java
@@ -1,12 +1,51 @@
 package com.tatocuervo.springbootstarter.lesson;
 
+import com.tatocuervo.springbootstarter.common.model.Lesson;
 import com.tatocuervo.springbootstarter.routes.Routes;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Api(tags = "Lessons")
 @RestController
 @RequestMapping(path = Routes.LESSONS)
 public class LessonController {
+
+    @ApiOperation(value = "Get all lessons within a course")
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Lesson> getLessons(@PathVariable long topicId, @PathVariable long courseId) {
+        return new ArrayList<>();
+    }
+
+    @ApiOperation("Get lesson within a course and topic")
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Lesson getLessonById(@PathVariable long topicId, @PathVariable long courseId, @PathVariable long id) {
+        return null;
+    }
+
+    @ApiOperation("Creates new lesson within a course and topic")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void addLesson(@PathVariable long topicId, @PathVariable long courseId, @RequestBody Lesson lesson) {
+
+    }
+
+    @ApiOperation("Updates a lesson within a course and topic")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateLesson(@PathVariable long topicId, @PathVariable long courseId, @PathVariable long id, @RequestBody Lesson lesson) {
+
+    }
+
+    @ApiOperation("Deletes a lesson within a course and topic")
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping(path = "/{id}")
+    public void deleteLesson(@PathVariable long topicId, @PathVariable long courseId, @PathVariable long id) {
+
+    }
 }
